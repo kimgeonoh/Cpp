8395c5326d0e1c2f72ae6bf4505451ce71a0bc55
study_electronic <84219715+kimgeonoh@users.noreply.github.com>
2023-03-19 23:31:12 +0900

    Create v[]_declare.cpp

---
+++ vector/v[]_declare.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<int> v[10];
+    // v.push_back(1);  불가능
+    v[1].push_back(1);
+    
+    /*for(int i=0; i<v.size(); i++){  불가능
+    }*/
+    for(int i=0; i<10; i++){
+    }
+    for(int i=0; i<v[1].size(); i++){ // 1 인덱스 안에 여러 원소가 들어있는 경우 
+    }
+    
+    // cout << v[0]; 불가능. 인덱스를 사용해 v를 선언한 경우 인덱스를 이용한 정보 저장용으로 사
+    
+}
