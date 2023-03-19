518fc62de910c485ea17943165ae023b9bd11b90
study_electronic <84219715+kimgeonoh@users.noreply.github.com>
2023-03-19 23:22:44 +0900

    Create test_debugging.cpp

---
+++ vector/test_debugging.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<int> v1;
+    vector<int> v2(5);
+    vector<int> v3(5,1);
+    vector<int> v4[5];
+    
+    vector<vector<int>> v5;
+    vector<vector<int>> v6(5);
+    vector<vector<int>> v7(5,vector<int>(5));
+    vector<vector<int>> v8[5];
+    
+    vector<pair<int, int>> v9;
+    vector<pair<int, int>> v10(5);
+    vector<pair<int, int>> v11(5,{1,1});
+    vector<pair<int, int>> v12[5];
+    
+    v4.push_back(1);
+
+    /*v1.push_back(1);
+    v2.push_back(1);
+    v3.push_back(1);
+    v4.push_back(1);
+    
+    v5.push_back(1);
+    v6.push_back(1);
+    v7.push_back(1);
+    v8.push_back(1);
+    
+    v9.push_back(1);
+    v10.push_back(1);
+    v11.push_back(1);
+    v12.push_back(1);
+    
+    cout << v1 << '\n';
+    cout << v2 << '\n';
+    cout << v3 << '\n';
+    cout << v4 << '\n';
+    
+    cout << v5 << '\n';
+    cout << v6 << '\n';
+    cout << v7 << '\n';
+    cout << v8 << '\n';
+    
+    cout << v9 << '\n';
+    cout << v10 << '\n';
+    cout << v11 << '\n';
+    cout << v12 << '\n';*/
+    
+    /*v.pop_back();
+    v.empty();
+    v.size;
+    cout << v[i];
+    v.begin();*/
+    
+}
