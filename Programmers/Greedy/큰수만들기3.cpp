bfe148cffe7e0f7f21e0d6263e427a40bf42f478
study_electronic <84219715+kimgeonoh@users.noreply.github.com>
2023-04-13 00:02:35 +0900

    Create 큰수만들기3.cpp

---
+++ Programmers/Greedy/큰수만들기3.cpp
@@ -0,0 +1,20 @@
+#include <string>
+
+using namespace std;
+
+string solution(string number, int k) {
+	string answer;
+    int idx = -1;
+    
+    for(int i=0; i<number.size()-k; i++){ // answer에 넣어야하는 수, number.size()-k 개
+        char maxV = ' ';
+        for(int j=idx+1; j<=k+i; j++){
+            if(maxV < number[j]){
+                maxV = number[j];
+                idx = j;
+            }
+        }
+        answer += maxV;
+    }
+	return answer;
+}
