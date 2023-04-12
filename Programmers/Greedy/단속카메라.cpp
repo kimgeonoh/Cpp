2e8c003b6c618df3a81c0080dff2da68c69187ea
study_electronic <84219715+kimgeonoh@users.noreply.github.com>
2023-04-12 22:29:45 +0900

    Create 단속카메라.cpp

---
+++ Programmers/Greedy/단속카메라.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(vector<int> a, vector<int> b){
+    return a.at(1) < b.at(1);
+}
+
+int solution(vector<vector<int>> routes){
+    int answer = 0;
+    int visit[10000]{};
+    int temp;
+    
+    sort(routes.begin(), routes.end(), cmp);
+    
+    for(int i=0; i<routes.size(); i++){
+        if(visit[i]) continue;
+        visit[i]=1;
+        answer++;
+        temp = routes[i][1];
+        
+        for(int j=i; j<routes.size(); j++){
+            if(visit[j]) continue;
+            if(routes[j][0] <= temp && temp <= routes[j][1]) visit[j] = 1;
+        }
+    }
+    
+    
+    return answer;
+}
