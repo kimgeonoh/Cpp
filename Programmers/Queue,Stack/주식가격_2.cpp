fc8b2f9de13df061b3244bdbd72352b7ce868e6a
study_electronic <84219715+kimgeonoh@users.noreply.github.com>
2023-04-17 11:12:19 +0900

    Update 주식가격_2.cpp

--- Programmers/Queue,Stack/주식가격_2.cpp
+++ Programmers/Queue,Stack/주식가격_2.cpp
@@ -12,7 +12,7 @@ vector<int> solution(vector<int> prices) {
     for(int i = 0; i<n-1; i++){
         int j = 0;
         for(j = i+1; j<n; j++){
-            if(prices[i] > prices[j]){
+            if(prices[i] > prices[j]){ // 작은 수가 나왔을 경우
                 j++;
                 break;
             }
@@ -20,6 +20,6 @@ vector<int> solution(vector<int> prices) {
         answer.push_back(j-i-1);
     }
     
-    answer.push_back(0);
+    answer.push_back(0); // 마지막 예외 처리
     return answer;
 }
