9c471c0b32540f683105bf250ae5bd015284a019
study_electronic <84219715+kimgeonoh@users.noreply.github.com>
2023-07-12 17:01:12 +0900

    Create 스쿠터검증.cpp

---
+++ SWEA/스쿠터검증.cpp
@@ -0,0 +1,22 @@
+#include <bist/stdc++.h>
+
+using namespace std;
+
+int T,answer;
+vector<int> v[9][9];
+
+void test1(){
+        
+    }
+    test2() 
+    test3()
+
+int main() {
+    cin >> T;
+    for(int t=1; t<=T; t++){
+        test1(); // 세로
+        test2(); // 가로
+        test3(); // 3*3
+        cout << '#' << t << ' ' << answer; // 정답일떄 1
+    }
+}
