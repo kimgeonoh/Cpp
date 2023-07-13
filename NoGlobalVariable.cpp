71aaaa5f3d5e3b2a1decc7ad898d9b27a0f3f579
study_electronic <84219715+kimgeonoh@users.noreply.github.com>
2023-07-13 15:56:38 +0900

    Create NoGlobalVariable.cpp

---
+++ NoGlobalVariable.cpp
@@ -0,0 +1,21 @@
+// INPUT 함수에서 문자 2개를 입력받으세요
+//OUTPUT 함수에서 입력 받은 문자 2개를 출력하세요.
+//main함수에서는 INPUT 함수와 OUTPUT함수를 호출 하시면 됩니다.
+
+#include <iostream>
+
+void INPUT(char& ch1, char& ch2) {
+    std::cin >> ch1 >> ch2;
+}
+
+void OUTPUT(char ch1, char ch2) {
+    std::cout << ch1 << " " << ch2 << std::endl;
+}
+
+int main() {
+    char input1, input2;
+    INPUT(input1, input2);
+    OUTPUT(input1, input2);
+
+    return 0;
+}
