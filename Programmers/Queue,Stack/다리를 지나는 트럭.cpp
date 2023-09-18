77423422edecb29153d66465d05dc7293925a31c
study_electronic <84219715+kimgeonoh@users.noreply.github.com>
2023-09-18 14:53:17 +0900

    Update 다리를 지나는 트럭.cpp

--- Programmers/Queue,Stack/다리를 지나는 트럭.cpp
+++ Programmers/Queue,Stack/다리를 지나는 트럭.cpp
@@ -2,36 +2,43 @@
 #include <vector>
 #include <queue>
 #include <deque>
+#include <iostream>
 
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int time = 0;
-    queue<int> bridge; // time
+    queue<pair<int,int>> bridge; // {weight, time}
     deque<int> wait;   // weight
     for(auto v : truck_weights) wait.push_back(v);
     int bridge_weight = 0;
     
-    while(!bridge.empty() && !wait.empty()){
+    while(!bridge.empty() || !wait.empty()){
         
-        // limit 당 다리에 트럭을 싣는다
-        while(bridge.size() < bridge_length && bridge_weight < weight && (bridge_weight + wait.front()) < weight ){
-            bridge.push(wait.front());
-            bridge_weight += wait.front();
-            wait.pop_front();
-        }
-        
-        // 트럭의 시간이 경과한다, 다리에서 시간이 다 지난것은 없앤다
+        // 트럭의 1시간이 경과한다, 다리에서 시간이 다 지난것은 없앤다
         int rep = bridge.size();
         while(rep--){
-            int now = bridge.front(); bridge.pop();
-            if(now > 2) continue;
-            now += 1;
-            bridge.push(now);
-            // brdige_weight 를 빼자
+            pair now = bridge.front(); bridge.pop();
+            int next = now.second + 1;
+            if(next > bridge_length) {
+                bridge_weight -= now.first;
+                continue;
+            }
+            bridge.push({now.first, next});
         }
+        
+        // limit 당 다리에 트럭을 싣는다
+        if(!wait.empty()) {
+            if(bridge.size() < bridge_length && (bridge_weight + wait.front()) <= weight){
+                bridge.push({wait.front(), 1});
+                bridge_weight += wait.front();
+                wait.pop_front();
+            }
+        }
+        
         // 시간이 지났다
-        time++;
+        time += 1;
+        //cout << time << '\n';
     }
     
     return time;
